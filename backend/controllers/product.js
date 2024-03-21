05b8506d240c04d7180f34f60ede41107dd0d624
irinagrishkevich <bonivomal1996@gmail.com>
2024-03-21 19:24:15 +0100

    add get post put delete method

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -6,6 +6,26 @@ class ProductController{
         const products = await ProductModel.findAll()
         res.send(products)
     }
+    static async getProduct(req, res){
+        const {id} = req.params;
+        const product = await ProductModel.find(id)
+        res.send(product)
+    }
+    static async addProduct(req, res){
+
+        const product = await ProductModel.create(req.body)
+        res.send(product)
+    }
+    static async changeProduct(req, res){
+        const {id} = req.params;
+        const product = await ProductModel.change(id, req.body)
+        res.send(product)
+    }
+    static async deleteProduct(req, res){
+        const {id} = req.params;
+        const product = await ProductModel.delete(id)
+        res.send({})
+    }
 }
 
 module.exports = ProductController
--- backend/models/product.js
+++ backend/models/product.js
@@ -1,14 +1,159 @@
 const fs = require('fs');
 const path = require('path');
 
-class ProductModel{
+class ProductModel {
+    static path = path.join(__dirname, '../db', 'products.json')
+
     static async findAll() {
-        return new Promise((resolve, reject)=>{
-            fs.readFile(path.join(__dirname, '../db', 'products.json'), 'utf8', (err, data)=>{
+        return new Promise((resolve, reject) => {
+            fs.readFile(this.path, 'utf8', (err, data) => {
                 if (err) {
                     reject(err);
                 } else {
                     resolve(JSON.parse(data));
+                }
+            })
+        })
+    }
+
+    static async find(id) {
+        return new Promise((resolve, reject) => {
+            fs.readFile(this.path, 'utf8', (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    try {
+                        const products = JSON.parse(data);
+                        const product = products.find(item => parseInt(item.id) === parseInt(id))
+                        if (product) {
+                            resolve(product);
+                        } else {
+                            reject(err)
+                        }
+
+                    } catch (e) {
+                        reject('Not a valid product or data object');
+                    }
+
+                    resolve();
+                }
+            })
+        })
+    }
+    static async find(id) {
+        return new Promise((resolve, reject) => {
+            fs.readFile(this.path, 'utf8', (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    try {
+                        const products = JSON.parse(data);
+                        const product = products.find(item => parseInt(item.id) === parseInt(id))
+                        if (product) {
+                            resolve(product);
+                        } else {
+                            reject(err)
+                        }
+
+                    } catch (e) {
+                        reject('Not a valid product or data object');
+                    }
+
+                    resolve();
+                }
+            })
+        })
+    }
+    static async create(newProduct) {
+        return new Promise((resolve, reject) => {
+            fs.readFile(this.path, 'utf8', (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    try {
+                        const products = JSON.parse(data);
+                       products.push(newProduct)
+                        const newJSON = JSON.stringify(products)
+                        fs.writeFile(this.path,newJSON,'utf-8', (error, data)=>{
+                            if (error){
+                                reject(error)
+                            } else {
+                                resolve(newProduct)
+                            }
+                        })
+
+                    } catch (e) {
+                        reject('Not a valid product or data object');
+                    }
+
+                    resolve();
+                }
+            })
+        })
+    }
+    static async change(id,newProduct) {
+        return new Promise((resolve, reject) => {
+            fs.readFile(this.path, 'utf8', (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    try {
+                        const products = JSON.parse(data);
+                        const productIndex = products.findIndex(item => parseInt(item.id) === parseInt(id))
+                        if (productIndex > -1) {
+                            products[productIndex] = newProduct
+
+                            const newJSON = JSON.stringify(products)
+                            fs.writeFile(this.path,newJSON,'utf-8', (error, data)=>{
+                                if (error){
+                                    reject(error)
+                                } else {
+                                    resolve(newProduct)
+                                }
+                            })
+                        } else {
+                            reject(err)
+                        }
+
+
+                    } catch (e) {
+                        reject('Not a valid product or data object');
+                    }
+
+                    resolve();
+                }
+            })
+        })
+    }
+    static async delete(id) {
+        return new Promise((resolve, reject) => {
+            fs.readFile(this.path, 'utf8', (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    try {
+                        const products = JSON.parse(data);
+                        const productIndex = products.findIndex(item => parseInt(item.id) === parseInt(id))
+                        if (productIndex > -1) {
+                            products.splice(productIndex, 1)
+                            const newJSON = JSON.stringify(products)
+                            fs.writeFile(this.path,newJSON,'utf-8', (error, data)=>{
+                                if (error){
+                                    reject(error)
+                                } else {
+                                    resolve()
+                                }
+                            })
+                        } else {
+                            reject(err)
+                        }
+
+
+                    } catch (e) {
+                        reject('Not a valid product or data object');
+                    }
+
+                    resolve();
                 }
             })
         })
